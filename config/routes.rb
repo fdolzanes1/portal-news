7b014665caa4cefddc1ad2323e43513b09780b94
fdolzanes1 <fdolzanes1@gmail.com>
2022-02-21 18:31:58 -0400

    added pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
-  root 'articles#index'
-
+  
+  root 'pages#home'
+  
+  get '/home', to: 'pages#home'
+  get '/contact', to: 'pages#contact'
+  get '/about', to: 'pages#about'
+  
   resources :articles
 end
