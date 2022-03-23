e849c157c4a70bc66900e3e4d93c97c5955917d1
Fabiano <fdolzanes1@gmail.com>
2022-03-23 07:35:05 -0400

    Add new users signup form

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
   get '/home', to: 'pages#home'
   get '/contact', to: 'pages#contact'
   get '/about', to: 'pages#about'
-  
+  get '/signup', to: 'users#new'
+
   resources :articles
+  resources :users, except: [:new]
 end
