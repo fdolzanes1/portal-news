205feebfc570120fdbc4613b51daf318d126cd1f
Fabiano <fdolzanes1@gmail.com>
2022-04-09 21:19:38 -0400

    Add boolean admin user field to app

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_22_211908) do
+ActiveRecord::Schema.define(version: 2022_04_10_011640) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(version: 2022_03_22_211908) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "password_digest"
+    t.boolean "admin", default: false
   end
 
 end
